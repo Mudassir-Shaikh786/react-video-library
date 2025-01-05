218832e63b73615a011af1b758b82d86bb18d899
MudassirShaikh786 <mudassirshaikh9768@gmail.com>
2025-01-05 20:51:51 +0530

    redux added

--- src/components/saved-video.jsx
+++ src/components/saved-video.jsx
@@ -1,29 +1,13 @@
-import { useState, useEffect } from "react";
+import store from "../store/store";
 
 export function SavedVideo(){
-
-    const[videoSlicer, setVideoSlicer]= useState([[{VideoId:0, Title:'', Url:'', Likes:0, Dislikes:0, Views:0, CategoryId:0}]]);
-
+        
     return(
-      <div className="container-fluid">
-        <button data-bs-toggle="modal" data-bs-target="#cart">show cart</button>
-        <div className="modal fade" id="cart"> 
-        <div className="modal-dialog"> 
-        <div className="modal-content"> 
-        <div className="modal-header"> 
-        <h3 className="text-primary">Your Cart Items</h3> 
-        <button className="btn btn-close" 
-        data-bs-dismiss="modal"></button> 
-    </div> 
-
-    <div className="modal-body"> 
-        <table className="table table-hover"> 
-   
-    <tbody> 
+      <div className="container-fluid d-flex flex-wrap">
     
         {
-            videoSlicer.map(video=>
-                <div key={video.VideoId} className="card m-2 ms-4 p-2 w-25">
+                 store.getState().store.videos.map(video=>
+                <div key={video.Videoid} className="card m-2 ms-4 p-2 w-25">
                     <iframe src={video.Url} width="100%" height="300"></iframe>
                     <div className="card-header">
                         <h6>{video.Title}</h6>
@@ -37,12 +21,6 @@ export function SavedVideo(){
             )
         }
     
-    </tbody> 
-      </table> 
-     </div> 
-    </div> 
-   </div> 
   </div> 
- </div> 
     ) 
     }
